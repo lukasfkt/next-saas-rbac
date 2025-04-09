b0a7671656d3775229862150f81ca31eeaae02e2
lucas.felipe <lucas.felipe@abix.com.br>
2025-04-09 20:13:09 -0300

    Adding zod to project

---
+++ packages/auth/src/subjects/project.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const projectSubject = z.tuple([
+    z.union([
+        z.literal('manage'),
+        z.literal('get'),
+        z.literal('update'),
+        z.literal('create'),
+        z.literal('delete')
+    ]),
+    z.literal('Project')
+])
+
+
+export type ProjectSubject = z.infer<typeof projectSubject>

b015272acd97ac2b79979ac173b8ee889728fe2f
Lucas Tanaka <lucas.felipe@abix.com.br>
2025-07-03 17:40:33 -0300

    Add invites routes

---
+++ apps/api/src/http/routes/invites/get-invite.ts
@@ -0,0 +1,72 @@
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '../../../lib/prisma'
+import { BadRequestError } from '../../_errors/bad-request-error'
+
+export async function getInvite(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/invites/:inviteId',
+    {
+      schema: {
+        tags: ['invites'],
+        summary: 'Get invite by id',
+        params: z.object({
+          inviteId: z.string().uuid(),
+        }),
+        response: {
+          200: z.object({
+            id: z.string().uuid(),
+            email: z.string().email(),
+            role: roleSchema,
+            createdAt: z.date(),
+            author: z
+              .object({
+                id: z.string().uuid(),
+                name: z.string().nullable(),
+                avatarUrl: z.string().url().nullable(),
+              })
+              .nullable(),
+            organization: z.object({
+              name: z.string(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { inviteId } = request.params
+      const invite = await prisma.invite.findUnique({
+        where: {
+          id: inviteId,
+        },
+        select: {
+          id: true,
+          email: true,
+          role: true,
+          createdAt: true,
+          author: {
+            select: {
+              id: true,
+              name: true,
+              avatarUrl: true,
+            },
+          },
+          organization: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      })
+
+      if (!invite) {
+        throw new BadRequestError('Invite not found')
+      }
+
+      return reply.send(invite)
+    },
+  )
+}

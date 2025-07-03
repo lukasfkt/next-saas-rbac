19087a23c19650fcfd692dbcd5fa8af9821fe5fc
Lucas Tanaka <lucas.felipe@abix.com.br>
2025-07-03 17:40:23 -0300

    Add members routes

---
+++ apps/api/src/http/routes/members/update-member.ts
@@ -0,0 +1,67 @@
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '../../../lib/prisma'
+import { getUserPermissions } from '../../../utils/get-user-permissions'
+import { UnauthorizedError } from '../../_errors/unauthorized-error'
+import { auth } from '../../middlewares/auth'
+
+export async function updateMember(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .put(
+      '/organizations/:slug/members/:memberId',
+      {
+        schema: {
+          tags: ['members'],
+          summary: 'Update a specific member',
+          security: [
+            {
+              bearerAuth: [],
+            },
+          ],
+          params: z.object({
+            slug: z.string(),
+            memberId: z.string().uuid(),
+          }),
+          body: z.object({
+            role: roleSchema,
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, memberId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('update', 'User')) {
+          throw new UnauthorizedError(
+            'You are not allowed to update this member',
+          )
+        }
+
+        const { role } = request.body
+
+        await prisma.member.update({
+          where: {
+            id: memberId,
+            organizationId: organization.id,
+          },
+          data: {
+            role,
+          },
+        })
+
+        reply.status(204).send()
+      },
+    )
+}
